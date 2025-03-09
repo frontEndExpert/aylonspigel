66733dae837e4f9d4b5a501bfbf543b26e3b108c
Aylon Spigel <aylon.spigel@gmail.com>
2025-03-10 00:13:23 +0200

    new way

--- src/pages/api/tiktok-verification.js
+++ src/pages/api/tiktok-verification.js
@@ -1,8 +1,1 @@
-export default function handler(req, res) {
-  res.setHeader("Content-Type", "text/plain")
-  res
-    .status(200)
-    .send(
-      "tiktok-developers-site-verification=34bhZfkTmZkBvcQnkVwwJVKe0bIFd6cw"
-    )
-}
+
