2b960a529aaafdc434192fe3aa114e6a0f3189d7
Aylon Spigel <aylon.spigel@gmail.com>
2025-03-10 00:22:43 +0200

    jsx way

---
+++ src/pages/tiktok34bhZfkTmZkBvcQnkVwwJVKe0bIFd6cw.jsx
@@ -0,0 +1,7 @@
+export default function TiktokVerification() {
+  return 'tiktok-developers-site-verification=34bhZfkTmZkBvcQnkVwwJVKe0bIFd6cw'
+}
+
+export const config = {
+  unstable_runtimeJS: false
+} 
