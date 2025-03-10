7a8bc535e8dfb9545d28cddba4662e560258943f
Aylon Spigel <aylon.spigel@gmail.com>
2025-03-10 23:41:19 +0200

    tiktok txt 3

--- next.config.js
+++ next.config.js
@@ -1,11 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ["js", "jsx", "txt"],
+  pageExtensions: ["js", "jsx"],
   output: "export",
   distDir: "_static",
   trailingSlash: true,
   images: {
     unoptimized: true
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.txt$/,
+      type: "asset/source"
+    })
+    return config
   }
 }
 module.exports = nextConfig
