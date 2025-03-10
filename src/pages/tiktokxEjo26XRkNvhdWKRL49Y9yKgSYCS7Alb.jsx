7341680c0d5c0c032325f0d5a81ea696f8629a8d
Aylon Spigel <aylon.spigel@gmail.com>
2025-03-10 22:46:39 +0200

    global scss 4

---
+++ src/pages/tiktokxEjo26XRkNvhdWKRL49Y9yKgSYCS7Alb.jsx
@@ -0,0 +1,7 @@
+export default function TiktokVerification() {
+    return 'tiktok-developers-site-verification=xEjo26XRkNvhdWKRL49Y9yKgSYCS7Alb'
+  }
+  
+  export const config = {
+    unstable_runtimeJS: false
+  } 
