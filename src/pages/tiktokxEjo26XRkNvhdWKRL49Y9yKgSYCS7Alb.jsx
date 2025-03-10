64406d0cc973c4ded45c7cd511f859dfe4056bb9
Aylon Spigel <aylon.spigel@gmail.com>
2025-03-10 23:45:19 +0200

    tiktok txt 3

--- src/pages/tiktokxEjo26XRkNvhdWKRL49Y9yKgSYCS7Alb.jsx
+++ src/pages/tiktokxEjo26XRkNvhdWKRL49Y9yKgSYCS7Alb.jsx
@@ -1,1 +1,8 @@
- 
+export default function TiktokVerification() {
+  return 'tiktok-developers-site-verification=xEjo26XRkNvhdWKRL49Y9yKgSYCS7Alb'
+}
+
+// Disable JS for this page
+export const config = {
+  unstable_runtimeJS: false
+} 
