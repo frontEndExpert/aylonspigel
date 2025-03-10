82d73a4eee5f7e8a0d79387bb6fd8b822876dc8c
Aylon Spigel <aylon.spigel@gmail.com>
2025-03-10 23:29:57 +0200

    tiktok txt 2

--- src/pages/tiktok34bhZfkTmZkBvcQnkVwwJVKe0bIFd6cw.jsx
+++
@@ -1,7 +0,0 @@
-export default function TiktokVerification() {
-  return 'tiktok-developers-site-verification=34bhZfkTmZkBvcQnkVwwJVKe0bIFd6cw'
-}
-
-export const config = {
-  unstable_runtimeJS: false
-} 
--- src/pages/tiktokxEjo26XRkNvhdWKRL49Y9yKgSYCS7Alb.jsx
+++
@@ -1,7 +0,0 @@
-export default function TiktokVerification() {
-    return 'tiktok-developers-site-verification=xEjo26XRkNvhdWKRL49Y9yKgSYCS7Alb'
-  }
-  
-  export const config = {
-    unstable_runtimeJS: false
-  } 
